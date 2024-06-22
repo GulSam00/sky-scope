670ec57228f78d9e3e2617042e113703e3a02087
GulSam00 <gulsamcono@gmail.com>
2024-06-22 13:42:48 +0900

    feat : vercel analytics install

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { Analytics } from "@vercel/analytics/react"
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,5 +10,6 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 );
