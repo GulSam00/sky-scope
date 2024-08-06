693f64c532ee5674963d72f516402b2716044c06
GulSam00 <gulsamcono@gmail.com>
2024-08-06 19:32:12 +0900

    design : 에러 페이지 디자인

--- src/Page/ErrorPage/index.tsx
+++ src/Page/ErrorPage/index.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+
 import { loadedData } from '@src/Store/loadingStateSlice';
+import { styled } from 'styled-components';
 
 const ErrorPage = () => {
   const dispatch = useDispatch();
@@ -7,10 +10,46 @@ const ErrorPage = () => {
   dispatch(loadedData());
 
   return (
-    <div>
-      <h1>Error Page</h1>
-    </div>
+    <ErrorPageContainer>
+      <div className='error-message'>
+        <div>
+          <h1>500 Internal Server Error</h1>
+        </div>
+        <div>죄송합니다, 서버에 문제가 발생했습니다.</div>
+      </div>
+    </ErrorPageContainer>
   );
 };
 
 export default ErrorPage;
+
+const ErrorPageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 90dvh;
+
+  background: linear-gradient(to bottom, #78c1ff, #fff); /* 기본 배경색 */
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+
+  .error-message {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    justify-content: center;
+
+    font-size: 24px;
+    font-family: Arial, sans-serif;
+    text-align: center;
+    color: #333;
+    z-index: 10; /* 구름보다 위에 위치 */
+    background-color: rgba(255, 255, 255, 0.8);
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+`;
