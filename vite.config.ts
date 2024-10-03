cf3ff96ff4277b6c2cb0f80135c7f1577bcfa750
GulSam00 <gulsamcono@gmail.com>
2024-10-03 18:47:03 +0900

    feat : allow cors 확장 없이 proxy 설정으로 로컬 cors 이슈 해결

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,16 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api/naver': {
+      '/api/naver/info': {
         target: 'https://openapi.naver.com',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api\/naver/, ''),
+        // /api/naver/info를 제거하고 요청을 전달
+        rewrite: path => path.replace(/^\/api\/naver\/info/, ''),
+      },
+      '/api/naver/token': {
+        target: 'https://nid.naver.com/oauth2.0',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api\/naver\/token/, ''),
       },
     },
   },
